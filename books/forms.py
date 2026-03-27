dcd0d88fcda070b9d27905a69f2f4eb61272e8dc
Sudip Paudel <paudelsudip007@gmail.com>
2026-03-27 12:06:00 +0545

    Fix styling issues in forms

--- books/forms.py
+++ books/forms.py
@@ -3,20 +3,39 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Book
 
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'genre', 'description', 'is_read']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Book title'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Author name'}),
+            'title': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Book title'}
+            ),
+            'author': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Author name'}
+            ),
             'genre': forms.Select(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Short description (optional)'}),
-            'is_read': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control', 'rows': 3,
+                    'placeholder': 'Short description (optional)'
+                }
+            ),
+            'is_read': forms.CheckboxInput(
+                attrs={'class': 'form-check-input'}
+            ),
         }
 
+
+
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Email'}
+        )
+    )
 
     class Meta:
         model = User
@@ -24,6 +43,12 @@ class RegisterForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
+        self.fields['username'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Username'}
+        )
+        self.fields['password1'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Password'}
+        )
+        self.fields['password2'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Confirm Password'}
+        )
